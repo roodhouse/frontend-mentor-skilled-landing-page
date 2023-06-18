6d291c9a0b74c48936e929989f94aafdfcf7a2a4
roodhouse <rughjm@gmail.com>
2023-06-18 14:15:36 -0500

    top portion for tablet done

--- src/components/body/Top.jsx
+++ src/components/body/Top.jsx
@@ -5,7 +5,7 @@ import TabletImage from '../../assets/image-hero-tablet@2x.webp'
 function Top() {
   return (
     <div id='topContentWrapper' className='flex flex-col text-left'>
-        <div id="textWrapper">
+        <div id="textWrapper" className='md:max-w-[398px]'>
             <div id="heading" className='mb-[26px] font-["Plus_Jakarta_Sans"] text-[40px] leading-[50px] font-extrabold text-naval'>
                 <h2>Maximize skill, minimize budget</h2>
             </div>
@@ -16,9 +16,9 @@ function Top() {
                 <button>Get Started</button>
             </div>
         </div>
-        <div id='topImage'>
+        <div id='topImage' className='md:absolute md:w-[368px] md:h-[640px] md:overflow-hidden md:left-[400px] md:top-[-65px]'>
             <img src={TopImage} alt="Cofee drinker" className='md:hidden' />
-            <img src={TabletImage} alt="Cofee drinker" className='hidden md:block' />
+            <img src={TabletImage} alt="Cofee drinker" className='hidden md:block md:max-w-[640px] md:w-[640px]' />
         </div>
     </div>
   )
