058b64465388cd456fdc94e274b6259e3b7c498f
roodhouse <rughjm@gmail.com>
2023-06-18 12:41:29 -0500

    added tablet image and another div

--- src/components/body/Top.jsx
+++ src/components/body/Top.jsx
@@ -1,20 +1,24 @@
 import React from 'react'
-import TopImage from '../../assets/image-hero-mobile@2x.webp'
+import TopImage from '../../assets/image-hero-mobile@2x.webp';
+import TabletImage from '../../assets/image-hero-tablet@2x.webp'
 
 function Top() {
   return (
     <div id='topContentWrapper' className='flex flex-col text-left'>
-        <div id="heading" className='mb-[26px] font-["Plus_Jakarta_Sans"] text-[40px] leading-[50px] font-extrabold text-naval'>
-            <h2>Maximize skill, minimize budget</h2>
-        </div>
-        <div id="subLine" className='mb-6 font-["Plus_Jakarta_Sans"] text-base leading-[26px] font-medium text-silver'>
-            <p>Our modern courses across a range of in-demand skills will give you the knowledge you need to live the life you want.</p>
-        </div>
-        <div id="topButton" className='mb-[46px] rounded-[31.5px] text-white bg-gradient-to-b from-orangeGradientTwo from-0% to-orangeGradientOne to-100% px-[37px] py-[15px] w-[167px] text-center font-["Plus_Jakarta_Sans"] text-base leading-[26px] font-bold'>
-            <button>Get Started</button>
+        <div id="textWrapper">
+            <div id="heading" className='mb-[26px] font-["Plus_Jakarta_Sans"] text-[40px] leading-[50px] font-extrabold text-naval'>
+                <h2>Maximize skill, minimize budget</h2>
+            </div>
+            <div id="subLine" className='mb-6 font-["Plus_Jakarta_Sans"] text-base leading-[26px] font-medium text-silver'>
+                <p>Our modern courses across a range of in-demand skills will give you the knowledge you need to live the life you want.</p>
+            </div>
+            <div id="topButton" className='mb-[46px] rounded-[31.5px] text-white bg-gradient-to-b from-orangeGradientTwo from-0% to-orangeGradientOne to-100% px-[37px] py-[15px] w-[167px] text-center font-["Plus_Jakarta_Sans"] text-base leading-[26px] font-bold'>
+                <button>Get Started</button>
+            </div>
         </div>
         <div id='topImage'>
-            <img src={TopImage} alt="Cofee drinker" />
+            <img src={TopImage} alt="Cofee drinker" className='md:hidden' />
+            <img src={TabletImage} alt="Cofee drinker" className='hidden md:block' />
         </div>
     </div>
   )
