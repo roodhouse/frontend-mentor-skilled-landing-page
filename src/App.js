e536975ecf2c2062973c6fa2db1f745778c663e2
roodhouse <rughjm@gmail.com>
2023-06-20 11:37:08 -0500

    dt header done

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <div id='wrapperDiv'>
-        <div id='headerWrapper' className='mt-4 mx-4 mb-[38px] md:mt-6 md:mx-10 md:mb-[99px]'>
+        <div id='headerWrapper' className='mt-4 mx-4 mb-[38px] md:mt-6 md:mx-10 md:mb-[99px] xl:mx-[165px] xl:mb-[185px]'>
           <Header />
         </div>
         <div id='bodyWrapper'>
